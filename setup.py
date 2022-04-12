b03d3f79d76604472ae2c6933fa2f611dd1e911f
Aleksey Korshuk <ale-kor02@mail.ru>
2022-04-12 14:41:09 +0300

    feat: updates

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from setuptools import setup, find_packages
 with open("requirements.txt", "r") as f:
     requirements = f.read().splitlines()
 
-setup(name="huggingnft", install_requires=requirements, packages=find_packages())
+setup(name="huggingnft", install_requires=requirements, packages=find_packages(), include_package_data=True,
+      package_data={'huggingnft': ['cards/*.md']},)
