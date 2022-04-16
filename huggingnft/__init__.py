988e7c172586fe61dbae03ab664ffa86d644b9f9
Aleksey Korshuk <ale-kor02@mail.ru>
2022-04-16 20:48:43 +0300

    feat: updates

--- huggingnft/__init__.py
+++ huggingnft/__init__.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+TEMPLATE_DATASET_CARD_PATH = Path(__file__).parent.absolute() / 'cards' / 'DATASET_README.md'
 TEMPLATE_MODEL_CARD_PATH = Path(__file__).parent.absolute() / 'cards' / 'MODEL_README.md'
 TEMPLATE_SNGAN_CARD_PATH = Path(__file__).parent.absolute() / 'cards' / 'SNGAN_MODEL_README.md'
 TEMPLATE_LIGHTWEIGHT_CARD_PATH = Path(__file__).parent.absolute() / 'cards' / 'LIGHTWEIGHT_MODEL_README.md'
